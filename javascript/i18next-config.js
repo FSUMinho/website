c55d53a1c1647f52ab1485bc6ad118f1634844d2
Duarte Leitão <92164696+Duarte0903@users.noreply.github.com>
2022-12-05 22:30:05 +0000

    translation method defined

---
+++ javascript/i18next-config.js
@@ -0,0 +1,35 @@
+function updateContent() {
+    const elements = document.getElementsByClassName("i18nelement");
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      const k = element.getAttribute("data-i18n");
+      element.innerHTML = i18next.t(k);
+    }
+  }
+  
+  async function i18Loader() {
+    const langs = ["pt", "en"];
+    const jsons = await Promise.all(
+      langs.map((l) => fetch("lang/" + l + ".json").then((r) => r.json()))
+    );
+    const res = langs.reduce((acc, l, idx) => {
+      acc[l] = { translation: jsons[idx] };
+      return acc;
+    }, {});
+    await i18next.init({
+      lng: "pt",
+      debug: true,
+      resources: res
+    });
+    updateContent();
+    i18next.on("languageChanged", () => {
+      updateContent();
+    });
+    const langSelector = document.getElementById("langSelector");
+    langSelector.removeAttribute("disabled");
+    langSelector.addEventListener("change", (e) => {
+      i18next.changeLanguage(e.target.value);
+    });
+  }
+  
+  i18Loader();
