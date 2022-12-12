79ee603f57cb49744756579bdbd34e0faa321741
Duarte Leitão <92164696+Duarte0903@users.noreply.github.com>
2022-12-12 21:15:43 +0000

    minor fixes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -7,18 +7,4 @@ function getYears()
     years.innerHTML = yearsInBusiness;
 }
 
-function getNumCourses()
-{
-    var coursesElement = document.getElementById("courses");
-
-    fetch("./lang/pt.json")
-    .then(response => response.json())
-    .then(data => {
-        const courses = data.courses;
-        var numCourses = Object.keys(courses).length;
-        coursesElement.innerHTML = numCourses;
-    });
-}
-
 getYears();
-getNumCourses();
