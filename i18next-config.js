3b37da8ff239af1a753429cdf19e338c02d8e20a
Duarte Leitão <92164696+Duarte0903@users.noreply.github.com>
2022-12-12 22:27:32 +0000

    test

--- i18next-config.js
+++ i18next-config.js
@@ -10,7 +10,7 @@ function updateContent() {
   async function i18Loader() {
     const langs = ["pt", "en"];
     const jsons = await Promise.all(
-      langs.map((l) => fetch("/lang/" + l + ".json").then((r) => r.json()))
+      langs.map((l) => fetch("./lang/" + l + ".json").then((r) => r.json()))
     );
     const res = langs.reduce((acc, l, idx) => {
       acc[l] = { translation: jsons[idx] };
@@ -32,30 +32,4 @@ function updateContent() {
     });
   }
 
-  async function i18Loader2() {
-    const langs = ["pt", "en"];
-    const jsons = await Promise.all(
-      langs.map((l) => fetch("../lang/" + l + ".json").then((r) => r.json()))
-    );
-    const res = langs.reduce((acc, l, idx) => {
-      acc[l] = { translation: jsons[idx] };
-      return acc;
-    }, {});
-    await i18next.init({
-      lng: "pt",
-      debug: true,
-      resources: res
-    });
-    updateContent();
-    i18next.on("languageChanged", () => {
-      updateContent();
-    });
-    const langSelector = document.getElementById("langSelector");
-    langSelector.removeAttribute("disabled");
-    langSelector.addEventListener("change", (e) => {
-      i18next.changeLanguage(e.target.value);
-    });
-  }
-  
   i18Loader();
-  i18Loader2();
