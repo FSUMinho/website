a334a3bd74abbf3206fed0ccea6b43089bbe9ba1
Duarte Leitão <92164696+Duarte0903@users.noreply.github.com>
2022-12-17 18:31:43 +0000

    3d model

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,5 @@
+/* TEAM'S YEARS IN BUSINESS */
+
 function getYears()
 {
     var years = document.getElementById("years");
@@ -6,5 +8,34 @@ function getYears()
     var yearsInBusiness = Currentyear - 2020;
     years.innerHTML = yearsInBusiness;
 }
-
 getYears();
+
+/* SLIDER SCRIPT */
+
+$(document).ready(function($) {
+    $('.card-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      arrows: true,
+      responsive: [{
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+         breakpoint: 400,
+         settings: {
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+         }
+      }]
+  });
+});
