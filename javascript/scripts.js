3e1e16bab117fca8b8d094bc269ae73e0d40c16a
Duarte Leitão <92164696+Duarte0903@users.noreply.github.com>
2022-12-17 22:23:17 +0000

    news slider progress

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,32 +10,23 @@ function getYears()
 }
 getYears();
 
-/* SLIDER SCRIPT */
+/* NEWS SLIDER */
 
-$(document).ready(function($) {
-    $('.card-slider').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: false,
-      autoplaySpeed: 2000,
-      arrows: true,
-      responsive: [{
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-         breakpoint: 400,
-         settings: {
-            arrows: false,
-            slidesToShow: 1,
-            slidesToScroll: 1
-         }
-      }]
+$(document).ready(function(){
+  $(".autoplay").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }]  
   });
-});
+}); 
